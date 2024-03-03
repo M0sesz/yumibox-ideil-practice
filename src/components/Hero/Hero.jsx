bb6871c5e78b563ecabf830a5016d4a68ebe0bd2
Anton <rodrigescouy25@gmail.com>
2024-03-03 19:05:52 +0200

    test2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -60,7 +60,7 @@ export const Hero = () => {
             </TextHero>
             <HeroTextWrap>
               <HeroText2>сет 21</HeroText2>
-              <HeroButton type="button">934 Грн</HeroButton>
+              <HeroButton type="button">934 грн</HeroButton>
             </HeroTextWrap>
           </HeroBox>
         </SwiperSlide>
